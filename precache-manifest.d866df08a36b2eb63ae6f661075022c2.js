6c841441f20426ea66985f662cbf9f1abca5af83
Steven <steven.dolbey@gmail.com>
2020-02-12 11:43:58 -0500

    Updates

---
+++ precache-manifest.d866df08a36b2eb63ae6f661075022c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d03f8ada8f686a9786968daa9026db9",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "3a7c8f4beb09aa357c7c",
+    "url": "/dig4639-mobile-dev/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7c8f4beb09aa357c7c",
+    "url": "/dig4639-mobile-dev/static/js/main.d6a0721a.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
