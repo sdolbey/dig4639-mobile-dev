e9ccebc885d3ecba09b7ab00be3b4cc49a6994c8
Steven <steven.dolbey@gmail.com>
2020-02-17 12:19:36 -0500

    w7a work

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,7 +1,36 @@
 function Sum (a, b) {
-  return a + b
+  let result = undefined
+  if(typeof a == "number" && typeof b == "number") {
+    result = a+b
+  }
+  return result
 }
 
-console.log(Sum(2,3));
+function AddList (numbers) {
+  let result = undefined
+  const areNumbers = (currentValue) => typeof currentValue == "number"
+  if(numbers.length != 0 && Array.isArray(numbers) && numbers.every(areNumbers)) {
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++) {
+      sum += numbers[i]
+    }
+    result = sum
+  }
+  return result
+}
+
+function ContainsString (first, second) {
+  //Returns true if the first parameter has as a substring the second parameter, both of which must be strings.
+  let result = false
+  if (typeof first == "string" && typeof second == "string") {
+    if (first.includes(second)) {
+      result = true
+    }
+  }
+  return result
+}
+
 
 export { Sum };
+export { AddList };
+export { ContainsString };
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,7 +1,48 @@
-import { Sum } from './MyMath.js'
+import { Sum, AddList, ContainsString } from './MyMath.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+describe("Sum", () => {
+  test('Whether undefined is returned on invalid type', () => {
+    expect(Sum(1, "Test")).toBeUndefined()
+  })
+  test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3)
+  })
+  test('produces the sum of 10 + 20, whcih should be 30', () => {
+    expect(Sum(10, 20)).toBe(30)
+  })
+})
+
+describe("AddList", () => {
+  test('returns undefined when array is empty', () => {
+    let stuff = []
+    expect(AddList(stuff)).toBeUndefined()
+  })
+  test('returns undefined when array is not all numbers', () => {
+    let stuff = [1, 'c', 'beep', 12]
+    expect(AddList(stuff)).toBeUndefined()
+  })
+  test('returns undefined when array is not an array', () => {
+    let stuff = "beep"
+    expect(AddList(stuff)).toBeUndefined()
+  })
+  test('returns 6 when list has 1, 2, and 3', () => {
+    let stuff = [1, 2, 3]
+    expect(AddList(stuff)).toBe(6)
+  })
+})
+describe("ContainsString", () => {
+  test('returns true when first is beep and second is eep', () => {
+    expect(ContainsString("beep","eep")).toBe(true)
+  })
+  test('returns false when first string does not contain second string', () => {
+    expect(ContainsString("beep","boop")).toBe(false)
+  })
+  test('returns false when first string is not a string', () => {
+    expect(ContainsString(7,"boop")).toBe(false)
+  })
+  test('returns false when second string is not a string', () => {
+    expect(ContainsString("beep",7)).toBe(false)
+  })
 })
 
 // ICE 
